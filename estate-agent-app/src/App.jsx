c32cdfc694e9042f6610405b3aa47676e9e61fd1
Mohamed Afzal <168853634+Mohamed-Afzal0@users.noreply.github.com>
2025-12-23 19:28:04 +0530

    Added mui and commited some major defferences.

--- estate-agent-app/src/App.jsx
+++ estate-agent-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 
 function App() {
-
+  
   return (
     <>
-      <h1> Hello World! </h1>
+      <header className="App-header">
+        <h1> SmartMove </h1>
+      </header>
     </>
   )
 }
